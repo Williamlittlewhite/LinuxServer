bcd3038a44d222b6913779cc07f0f9cc87fb7ac4
bzx517291467 <bzx517291467@gmail.com>
2022-11-01 23:11:25 +0800

    2022.11.1

--- lesson04/library/src/sub.c
+++ lesson04/library/src/sub.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include "head.h"
 
 int subtract(int a, int b)
 {
--- lesson06/library/main.c
+++ lesson06/library/main.c
@@ -12,3 +12,9 @@ int main()
     printf("a / b = %f\n", divide(a, b));
     return 0;
 }
+
+//动态库制作gcc fpic -c xxx.c   gcc -shared xxx.o -o libxxx.so
+//使用动态库 gcc main.c -o main -I ./include/ -l calc -L ./lib/ 会报错
+//动态库依赖list dynamic dependencies检查动态库依赖 ldd main
+//
+/*当系统加载可执行代码时候，能够知道其所依赖的库的名字，但是还需要知道绝对路径。此时就需要系统的动态载入器来获取该绝对路径。对于elf格式的可执行程序，是由ld-linux.so来完成的，它先后搜索elf文件的 DT_RPATH段(虚拟地址空间用户不能改～) ——> 环境变量LD_LIBRARY_PATH(用户级即.bashrcsource .bashrc 系统级即sudo vim etc/profile 然后 source etc/profile) ——> /etc/ld.so.cache文件列表(sudo vim /etc/ld.so.conf然后sudo ldconfig) ——> /lib/，/usr/lib目录找到库文件后将其载入内存(这种不推荐用)*/
