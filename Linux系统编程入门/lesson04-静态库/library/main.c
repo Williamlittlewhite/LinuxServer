0ddf373c79de964f2eac60e60119376515c9ba79
bzx517291467 <bzx517291467@gmail.com>
2022-11-12 20:25:29 +0800

    2022.11.12

--- Linux系统编程入门/lesson04-静态库/library/main.c
+++ Linux系统编程入门/lesson04-静态库/library/main.c
@@ -13,6 +13,7 @@ int main()
     return 0;
 }
 
-#本节课分为测试源文件，lib文件夹里面放libxxx.a静态库文件，src文件夹里放入编写的源文件，include文件夹里放入头文件
-#编译本节课的内容的命令gcc main.c -o main -I ./include/ -l calc -L ./lib/
-#-o 起另外一个名  -I 指定头文件搜索文件夹 -l 指定使用的静态库 -L 指定使用的静态库的路径
+/*本节课分为测试源文件，lib文件夹里面放libxxx.a静态库文件，src文件夹里放入编写的源文件，include文件夹里放入头文件
+编译本节课的内容的命令gcc main.c -o main -I ./include/ -l calc -L ./lib/
+-o 起另外一个名  -I 指定头文件搜索文件夹 -l 指定使用的静态库 -L 指定使用的静态库的路径
+*/
